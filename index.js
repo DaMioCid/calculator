0f84a8fd666defab994bc6efe714110f29ef491b
David Orihuela <damiocid@gmail.com>
2017-05-15 20:21:59 -0400

    refactored

--- index.js
+++ index.js
@@ -1,132 +1,61 @@
-$(function () {
-    var $screen = $('#screen div'),
-        $p = $('p'),
+var calcApi = (function () {
+    var res,
         num = [],
         calc = [],
         ops = [],
         val, el, sym, full = false;
 
-    $p.mouseup(function () {
-        $(this).toggleClass('clicked');
-    }).mousedown(function () {
-        $(this).toggleClass('clicked');
-    });
-
-    $p.on('click', function () {
-        val = $(this).html();
-        if (!isNaN(val) || val === '.') {
-            ifNumbers(val);
-        } else if (val !== '=' && val !== 'C' && val !== 'AC') {
-            ifOperator(val, $(this));
-        } else if (val === '=' && calc.length !== 0) {
-            ifEqual();
-        } else if (val === 'C' || val === 'AC') {
-            calcApi.op(val);
-        }
-    });
-    function ifNumbers(val) {
-        //Set full to keep number to go over nine digits.
-        num.length < 9 ? full = false : full = true;
-        if (!full) {
-            if (val === '.' && !num.includes('.')) {
-                if (num[0] > 0) {
-                    num.push('.');
-                    calcApi.screen(num.join(''));
-                } else if (num.length === 0) {
-                    num.push(0);
-                    num.push('.');
-                    calcApi.screen(num.join(''));
-                }
-                calcApi.removeClass();
-            } else if (val === '0' && !num.includes('.') && num.length === 0) {
-                $screen.html('0');
-                calcApi.removeClass();
-            } else if (val !== '.') {
-                num.push(val);
-                calcApi.screen(num.join(''));
-                calcApi.removeClass();
-            }
-        }
-
-    }
-    function ifOperator(val, $this) {
-        ops.push(val);
-        //if number blocks abuse of multiple operator
-        if (!isNaN($screen.html()) && (el === undefined || !el.hasClass('active'))) {
-            //when operator clicked, push screen first number to array.
-            calc.push(parseFloat($screen.html()));
-        }
-        if (el !== undefined && el.hasClass('active')) {
-            ops.shift();
-            el.removeClass('active');
-        }
-        num = [];
-        el = $this;
-        el.addClass("active");
-        sym = val;
-        if (calc.length === 2) {
-            calcApi.operate(ops[0]);
-            ops.shift();
-        }
-    }
-    function ifEqual() {
-        //when equal clicked, push second number from screen to array
-        calc.push(parseFloat($screen.html()) || 0);
-        //selects operation and performs desired operation on the two numbers on array calc.
-        calcApi.operate(sym);
-        calc = [];
-        ops = [];
-    }
-    var calcApi = {
+    var core = {
         operate: function (sym) {
+            var z;
             switch (sym) {
                 case '+':
-                    calcApi.add(calc[0], calc[1]);
+                    z = this.add(calc[0], calc[1]);
                     break;
                 case '-':
-                    calcApi.sub(calc[0], calc[1]);
+                    z = this.sub(calc[0], calc[1]);
                     break;
                 case 'x':
-                    calcApi.mul(calc[0], calc[1]);
+                    z = this.mul(calc[0], calc[1]);
                     break;
                 case '/':
-                    calcApi.div(calc[0], calc[1]);
+                    z = this.div(calc[0], calc[1]);
                     break;
             }
+            return z;
         },
-        res: 0,
         add: function (a, b) {
             res = a + b;
-            calcApi.result(res);
+            return this.result(res);
         },
         sub: function (a, b) {
             res = a - b;
-            calcApi.result(res);
+            return this.result(res);
         },
         mul: function (a, b) {
             res = a * b;
-            calcApi.result(res);
+            return this.result(res);
         },
         div: function (a, b) {
             res = a / b;
-            calcApi.result(res);
+            return this.result(res);
         },
-        clearAll: function () {
+        clearAll: function (s) {
             num = [];
             calc = [];
             ops = [];
-            $screen.html('0');
+            s.html('0');
             $('p').removeClass('active');
         },
-        clear: function () {
+        clear: function (s) {
             num = [];
-            $screen.html('0');
+            s.html('0');
         },
-        op: function (x) {
+        op: function (x, s) {
             if (x === 'AC') {
-                calcApi.clearAll();
+                this.clearAll(s);
             } else if (x === 'C') {
-                calcApi.clear();
+                this.clear(s);
                 if (calc.length !== 0) {
                     el.addClass("active");
                 }
@@ -143,10 +72,7 @@ $(function () {
             num = [];
             calc = [];
             calc.push(x);
-            $screen.html(x);
-        },
-        screen: function (x) {
-            $screen.html(x);
+            return x;
         },
         removeClass: function () {
             if (el !== undefined && el.hasClass('active')) {
@@ -154,4 +80,91 @@ $(function () {
             }
         }
     };
-});
+
+    var publicApi = {
+        ifNumbers: function (val, s) {
+            //Set full to keep number to go over nine digits.
+            num.length < 9 ? full = false : full = true;
+            if (!full) {
+                if (val === '.' && !num.includes('.')) {
+                    if (num[0] > 0) {
+                        num.push('.');
+                        s.html(num.join(''));
+                    } else if (num.length === 0) {
+                        num.push(0);
+                        num.push('.');
+                        s.html(num.join(''));
+                    }
+                    core.removeClass();
+                } else if (val === '0' && !num.includes('.') && num.length === 0) {
+                    s.html('0');
+                    core.removeClass();
+                } else if (val !== '.') {
+                    num.push(val);
+                    s.html(num.join(''));
+                    core.removeClass();
+                }
+            }
+        },
+        ifOperator: function (val, $this, s) {
+            ops.push(val);
+            //if number blocks abuse of multiple operator
+            if (!isNaN(s.html()) && (el === undefined || !el.hasClass('active'))) {
+                //when operator clicked, push screen first number to array.
+                calc.push(parseFloat(s.html()));
+            }
+            if (el !== undefined && el.hasClass('active')) {
+                ops.shift();
+                el.removeClass('active');
+            }
+            num = [];
+            el = $this;
+            el.addClass("active");
+            sym = val;
+            if (calc.length === 2) {
+                s.html(core.operate(ops[0]));
+                ops.shift();
+            }
+        },
+        ifEqual: function (s) {
+            if (calc.length !== 0) {
+                //when equal clicked, push second number from screen to array
+                calc.push(parseFloat(s.html()) || 0);
+                //selects operation and performs desired operation on the two numbers on array calc.
+                s.html(core.operate(sym));
+                calc = [];
+                ops = [];
+            }
+        },
+        ifClear: function (val, s) {
+            core.op(val, s);
+        }
+    };
+    return publicApi;
+})();
+
+
+(function ($, calc) {
+
+    var $screen = $('#screen div'),
+        $p = $('p');
+
+    $p.mouseup(function () {
+        $(this).toggleClass('clicked');
+    }).mousedown(function () {
+        $(this).toggleClass('clicked');
+    });
+
+    $p.on('click', function () {
+        val = $(this).html();
+        if (!isNaN(val) || val === '.') {
+            calc.ifNumbers(val, $screen);
+        } else if (val !== '=' && val !== 'C' && val !== 'AC') {
+            calc.ifOperator(val, $(this), $screen);
+        } else if (val === '=') {
+            calc.ifEqual($screen);
+        } else if (val === 'C' || val === 'AC') {
+            calc.ifClear(val, $screen);
+        }
+    });
+})(jQuery, calcApi);
